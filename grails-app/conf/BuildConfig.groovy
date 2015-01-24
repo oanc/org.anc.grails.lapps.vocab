5f751ae8b36e245027fb619f8069022746a1704d
Keith Suderman <suderman@anc.org>
2015-01-24 13:31:01 -0500

    Fixed ANC Maven repositories.

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -35,12 +35,13 @@ grails.project.dependency.resolution = {
     repositories {
         inherits true // Whether to inherit repository definitions from plugins
 
+        mavenRepo("http://www.anc.org:8080/nexus/content/repositories/snapshots")
+        mavenRepo("http://www.anc.org:8080/nexus/content/repositories/releases")
         grailsPlugins()
         grailsHome()
         mavenLocal()
         grailsCentral()
         mavenCentral()
-        mavenRepo("http://www.anc.org:8080/nexus")
         // uncomment these (or add new ones) to enable remote dependency resolution from public Maven repositories
         //mavenRepo "http://repository.codehaus.org"
         //mavenRepo "http://download.java.net/maven/2/"
